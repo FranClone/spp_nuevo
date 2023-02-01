d8bb0cc7c3f32cc8f03ef3630795c90c48e60f7d
CristopherQuintana <crisquintana33@gmail.com>
2023-02-01 12:01:04 -0300

    DB conectada, se muestra con planificadores

--- core/views.py
+++ core/views.py
@@ -6,9 +6,11 @@ import pyodbc
 #está en inglés el video pero se ve bueno
 #me compiló ahora y las migraciones se hicieron
 #ojalá poder entrar al administrador
-
-#cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=;DATABASE=SPP;UID=sa;PWD=B3t3ch1tda.2021')
-#cursor = cnxn.cursor()
+try:
+    conexion = pyodbc.connect('DRIVER={SQL Server};SERVER=192.168.100.138,1434;DATABASE=SPP;UID=sa;PWD=B3t3ch1tda.2021')
+    print("conexión a base de datos exitosa")
+except Exception as ex:
+    print(ex)
 
 #cursor.execute("SELECT * FROM Bodega")
 
@@ -23,7 +25,6 @@ class Administracion(View): #HomeView da acceso a ambos, get req y post req. Get
         context={
 
         }
-
         return render(request, 'administracion.html', context) #Las '' son el template osea la info en html que se mostrará. También podria definirse context como doble break pero en este caso lo dejamos así, como variable
 
 class Carga_sv(View): 
@@ -117,32 +118,46 @@ class Header(View):
 #Vistas menu desplegable de header, planificador
 class Plan_Bodega(View): 
     def get(self, request, *args, **kwargs):
-        context={
-
-        }
-
-        return render(request, 'planificador/planificador_bodega.html', context)
+        cursor = conexion.cursor()
+        cursor.execute("""SELECT id_bodega, nombre_bodega, 
+                        rut_empresa, descripcion_bodega
+                        FROM BODEGA""")
+        rows = cursor.fetchall()
+        cursor.commit()
+        cursor.close()
+        return render(request, 'planificador/planificador_bodega.html', {'rows':rows})
 
 class Plan_Lineas(View): 
     def get(self, request, *args, **kwargs):
-        context={
-
-        }
-
-        return render(request, 'planificador/planificador_lineas.html', context)
+        cursor = conexion.cursor()
+        cursor.execute("""SELECT id_linea, nombre_linea, 
+                        rut_empresa, descripcion_linea
+                        FROM LINEA""")
+        rows = cursor.fetchall()
+        cursor.commit()
+        cursor.close()
+        return render(request, 'planificador/planificador_lineas.html', {'rows':rows})
 
 class Plan_Productos(View): 
     def get(self, request, *args, **kwargs):
-        context={
-
-        }
-
-        return render(request, 'planificador/planificador_productos.html', context)
+        cursor = conexion.cursor()
+        cursor.execute("""SELECT id_producto, nombre_producto, 
+                        descripcion_producto, espesor_producto,
+                        ancho_producto, largo_producto
+                        FROM PRODUCTO""")
+        rows = cursor.fetchall()
+        cursor.commit()
+        cursor.close()
+        return render(request, 'planificador/planificador_productos.html', {'rows':rows})
 
 class Plan_Rollizo(View): 
     def get(self, request, *args, **kwargs):
-        context={
-
-        }
-
-        return render(request, 'planificador/planificador_rollizo.html', context)
+        cursor = conexion.cursor()
+        cursor.execute("""SELECT id_rollizo, nombre_rollizo, 
+                        descripcion_rollizo, id_linea,
+                        diametro, rut_empresa
+                        FROM ROLLIZO""")
+        rows = cursor.fetchall()
+        cursor.commit()
+        cursor.close()
+        return render(request, 'planificador/planificador_rollizo.html', {'rows':rows})
