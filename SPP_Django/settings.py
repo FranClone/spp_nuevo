5a55b5cbb73b0750657777ad6017d1b2df7c14d8
CristopherQuintana <crisquintana33@gmail.com>
2023-05-04 15:43:33 -0400

    Host agregados

--- SPP_Django/settings.py
+++ SPP_Django/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','192.168.100.138','sppprod.servehttp.com']
 
 AUTH_USER_MODEL = 'asignaciones.UserProfile'
 
