7a21acc1b679f6387fb0c54ca4650b793fe56503
Nicolás <nicolas.iloyolar@gmail.com>
2023-06-14 21:08:21 -0400

    nico 14-06-2023

---
+++ core/static/js/filtro-productos.js
@@ -0,0 +1,93 @@
+$(document).ready(function() {
+    var $filterButton = $(".filter-button");
+    var $showFilterForm = $(".show-filter .filter-form");
+    var $advancedSearch = $("#advanced-search");
+    var $advancedSearchOptions = $("#advanced-search-options");
+    var $filterForm = $(".filter-form");
+    var $filterInputs = $(".filter-input");
+    var $clearFilterButton = $(".clear-filter-button");
+    var $tableRows = $(".table tbody tr");
+    var $noDataMessage = $(".no-data-message");
+    var $filterSelects = $(".filter-select");
+
+    $filterButton.click(function() {
+        $showFilterForm.toggle();
+    });
+
+    $advancedSearch.change(function() {
+        $advancedSearchOptions.toggle(this.checked);
+    });
+
+    $filterForm.submit(function(e) {
+        e.preventDefault();
+        filterTable();
+    });
+
+    $filterInputs.keyup(function() {
+        filterTable();
+    });
+
+    $clearFilterButton.click(function() {
+        $filterForm[0].reset();
+        $tableRows.show();
+        $noDataMessage.hide();
+    });
+
+    function filterTable() {
+        $tableRows.hide();
+
+        var filterBy = $filterSelects.filter("[name='filter-by']").val();
+        var filterValue = $filterInputs.filter("[name='filter-value']").val().toLowerCase();
+        var filterOption = $filterSelects.filter("[name='filter-option']").val();
+        var filterLength = parseFloat($("#filter-length").val());
+        var filterLengthMax = parseFloat($("#filter-length-max").val());
+        var filterWidth = parseFloat($("#filter-width").val());
+        var filterWidthMax = parseFloat($("#filter-width-max").val());
+        var filterThickness = parseFloat($("#filter-thickness").val());
+        var filterThicknessMax = parseFloat($("#filter-thickness-max").val());
+        var advancedSearch = $advancedSearch.is(":checked");
+
+        $tableRows.each(function() {
+            var $row = $(this);
+            var rowData = $row.find("td:nth-child(" + (getFieldIndex(filterBy) + 1) + ")").text().toLowerCase();
+            var rowLength = parseFloat($row.find("td:nth-child(5)").text());
+            var rowWidth = parseFloat($row.find("td:nth-child(4)").text());
+            var rowThickness = parseFloat($row.find("td:nth-child(3)").text());
+
+            if (advancedSearch) {
+                if (filterLength && (rowLength < filterLength || rowLength > filterLengthMax)) {
+                    return;
+                }
+                if (filterWidth && (rowWidth < filterWidth || rowWidth > filterWidthMax)) {
+                    return;
+                }
+                if (filterThickness && (rowThickness < filterThickness || rowThickness > filterThicknessMax)) {
+                    return;
+                }
+            }
+
+            if (filterOption === "partial" && rowData.includes(filterValue)) {
+                $row.show();
+            } else if (filterOption === "exact" && rowData === filterValue) {
+                $row.show();
+            }
+        });
+
+        $noDataMessage.toggle($tableRows.filter(":visible").length === 0);
+    }
+
+    function getFieldIndex(fieldName) {
+        var index = 0;
+
+        switch (fieldName) {
+            case "nombre":
+                index = 1;
+                break;
+            case "codigo":
+                index = 0;
+                break;
+        }
+
+        return index;
+    }
+});
