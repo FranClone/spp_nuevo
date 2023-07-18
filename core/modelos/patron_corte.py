93d2032da74b0ca60a52281fd5791f43806bd074
Nicolás <nicolas.iloyolar@gmail.com>
2023-07-18 15:38:19 -0400

    nico

--- core/modelos/patron_corte.py
+++ core/modelos/patron_corte.py
@@ -1,10 +1,21 @@
 from django.db import models
 
 class PatronCorte(models.Model):
-    codigo_patron = models.CharField(max_length=20, unique=True, null=False, blank=False, default='')
-    nombre_patron = models.CharField(max_length=20, null=False, blank=False)
+    """Este modelo define la entidad Patrón de Corte"""
+    
+    # Entradas 
+    codigo = models.CharField(max_length=20, unique=True, null=False, blank=False, default='')
+    nombre = models.CharField(max_length=20, null=False, blank=False)
+    descripcion = models.CharField(max_length=20, null=False, blank=False)
+    rollizo = models.CharField(max_length=10)
+    rendimiento = models.FloatField(max_length=5)
+    velocidad_linea = models.FloatField(max_length=5)
+    setup_time = models.FloatField(max_length=5)
+    lead_time = models.FloatField(max_length=5)
+
+    # Salidas
+    utilizado = models.BooleanField()
     producto_asociado = models.CharField(max_length=20, null=False, blank=False, default='')
-    clase_diametrica_rollizo = models.CharField(max_length=20, null=False, blank=False, default='')
 
     def __str__(self):
-        return self.nombre_patron
+        return f'Patrón de corte {self.codigo}: {self.rendimiento} %'
