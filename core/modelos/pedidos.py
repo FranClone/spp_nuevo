c4dbbb3e2b244c89ac60d212d04ff4e76c09e82b
f-silva-m <panchito.silva1501@gmail.com>
2023-07-19 14:55:19 -0400

    pancho

--- core/modelos/pedidos.py
+++ core/modelos/pedidos.py
@@ -13,6 +13,7 @@ class Pedido(models.Model):
     cantidad = models.PositiveIntegerField(null=False, blank=False)
     prioridad = models.IntegerField(null=False, blank=False)
     
+    
     # Salidas
     estado = models.CharField(max_length=20, null=False, blank=False)
 
