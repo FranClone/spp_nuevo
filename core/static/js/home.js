945aedb15f073d9fe2ca0f0157fb324194ce9a28
FabianSalinasC <fa.salinasc@duocuc.cl>
2023-09-01 13:41:29 -0400

    Commit 01/09/2023

---
+++ core/static/js/home.js
@@ -0,0 +1,97 @@
+function setCookie(name, value, days) {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+}
+function getCookie(name) {
+    const cookieName = `${name}=`;
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookieArray = decodedCookie.split(';');
+
+    for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(cookieName) === 0) {
+            return cookie.substring(cookieName.length, cookie.length);
+        }
+    }
+    return null;
+}
+
+// Obtener los IDs previamente vistos de la cookie
+const idsPreviosVistos = getCookie('ids_vistos') ? getCookie('ids_vistos').split(',') : [];
+
+// Realizar solicitud AJAX para obtener los IDs de pedidos desde el servidor
+function showWelcomePopup() {
+    // Obtener los IDs previamente vistos de la cookie
+    const idsPreviosVistos = getCookie('ids_vistos') ? getCookie('ids_vistos').split(',') : [];
+
+    // Realizar solicitud AJAX para obtener los IDs de pedidos desde el servidor
+    fetch('/obtener-ids-pedidos/')
+        .then(response => response.json())
+        .then(data => {
+            const idsNuevosDesdeBackend = data.ids_pedidos;
+
+            // Encontrar los IDs nuevos
+            const nuevosIDs = idsNuevosDesdeBackend.filter(id => !idsPreviosVistos.includes(id.toString()));
+
+            let hayNuevosPedidos = false;
+
+            if (nuevosIDs.length > 0) {
+                // Mostrar mensaje para los nuevos IDs
+                console.log(`¡Nuevos IDs encontrados! ${nuevosIDs.join(', ')}`);
+
+                // Agregar los nuevos IDs a la lista de IDs vistos
+                const idsVistosActualizados = idsPreviosVistos.concat(nuevosIDs);
+
+                // Actualizar la cookie con los IDs vistos
+                setCookie('ids_vistos', idsVistosActualizados.join(','), 365); // Guardar por 1 año
+
+                hayNuevosPedidos = true;
+            }
+
+            console.log(`¿Hay nuevos pedidos? ${hayNuevosPedidos}`);
+
+            const popupOverlay = document.getElementById('welcome-popup');
+            const popupContent = document.querySelector('.popup-content-pedidos');
+
+            if (hayNuevosPedidos) {
+                popupContent.innerHTML = `
+                    <h3>¡Bienvenido!</h3>
+                    <h4>¡Hay nuevos pedidos!</h4>
+                    <button id="btnActualizar" class="popup-update-button" onclick="actualizarPedidos()">Actualizar</button>
+                    <button class="popup-close-pedidos" onclick="hideWelcomePopup()">x</button>
+                `;
+            } else {
+                popupContent.innerHTML = `
+                    <h3>Existe una planificación anterior</h3>
+                    <h4>No hay pedidos nuevos</h4>
+                    <button id="btnActualizar" class="popup-update-button" onclick="actualizarPedidos()">Actualizar</button>
+                `;
+            }
+
+            popupOverlay.style.display = 'block';
+        })
+        .catch(error => {
+            console.error("Error al obtener los IDs de pedidos:", error);
+        });
+}
+
+function hideWelcomePopup() {
+    const popupOverlay = document.getElementById('welcome-popup');
+    popupOverlay.style.display = 'none';
+}
+
+window.onload = function () {
+    showWelcomePopup();
+};
+
+
+function actualizarPedidos() {
+    // Realiza cualquier lógica de actualización que necesites aquí
+
+    // Redirecciona al usuario a 'pantalla-carga'
+    window.location.href = '/pantalla-carga/';
+}
