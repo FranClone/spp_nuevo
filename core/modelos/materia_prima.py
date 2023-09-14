8aa826433472f0942d80108970985663177d9fe8
FranClone <franci.gonzalezs@duocuc.cl>
2023-09-14 00:03:24 -0300

    Modelos

--- core/modelos/materia_prima.py
+++ core/modelos/materia_prima.py
@@ -5,18 +5,20 @@ class MateriaPrima(models.Model):
     
     # Entradas
     #el atributo mas importante en este modelo es la clase_diametrica, la materia prima se clasifica por el numero de buzon, conicidad es la forma del tronco
-    nombre= models.CharField(max_length=20, null=False, blank=False)
-    volumen_llegada = models.PositiveIntegerField(null=False, blank=False)
-    procedencia = models.CharField(max_length=10)
-    cantidad = models.PositiveIntegerField(null=False, blank=False)
-    largo= models.PositiveIntegerField(null=False, blank=False)
-    clase_diametrica= models.PositiveIntegerField(null=False, blank=False) 
-    conicidad = models.FloatField(max_length=10)
+    numero_buzon = models.IntegerField()
     tipo_madera = models.CharField(max_length=10)
+    clase_diametrica = models.CharField(max_length=10)
+    largo = models.FloatField(max_length=10)
+    cantidad = models.FloatField(max_length=10)
+    conicidad = models.FloatField(max_length=10)
+    linea_produccion = models.CharField(max_length=10)
+    costo_almacenamiento = models.FloatField(max_length=10) # obviar por el momento
+    inventario_inicial = models.FloatField(max_length=10)
+    
     # Salidas
     volumen_procesado = models.FloatField(max_length=10)
     inventario_final = models.FloatField(max_length=10)
         
     def __str__(self):
-        return f'MateriaPrima {self.nombre}: {self.volumen_llegada} m3'
+        return f'Buzón {self.numero_buzon}: {self.cantidad} m3'
 
--- core/modelos/producto.py
+++ core/modelos/producto.py
@@ -11,13 +11,13 @@ class Producto(models.Model):
     ancho = models.DecimalField(max_digits=5, decimal_places=2, null=False, blank=False)    
     alto = models.DecimalField(max_digits=5, decimal_places=2, null=False, blank=False)
     demanda = models.CharField(max_length=100, null=False, blank=False)
-    inventario_bodega_inicial = models.FloatField(max_length=10)
+    inventario_inicial = models.FloatField(max_length=10)
     valor_inventario = models.FloatField(max_length=10)
     costo_almacenamiento = models.FloatField(max_length=10)
-    nombre = models.ForeignKey('PatronCorte', on_delete=models.CASCADE)     
+    
     # Salidas
     volumen_obtenido = models.FloatField(max_length=10)
-    inventario_producto_final = models.FloatField(max_length=10)
+    inventario_final = models.FloatField(max_length=10)
     
     def __str__(self):
         return f' {self.nombre}'
