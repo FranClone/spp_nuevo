eb791dbb94c98aa3d313ceac04a56e5d13ab2843
FabianSalinasC <141871095+FabianSalinasC@users.noreply.github.com>
2023-11-05 18:38:32 -0300

    Revert "cambios "

--- core/modelos/rendimiento_producto.py
+++ core/modelos/rendimiento_producto.py
@@ -5,7 +5,7 @@ from .producto import Producto
 class Rendimiento_Producto(models.Model):
     patron_id = models.ForeignKey(PatronCorte, on_delete=models.CASCADE)
     producto_id = models.ForeignKey(Producto, on_delete=models.CASCADE)
-    rendimiento = models.IntegerField(null=False, blank=True) 
+    rendimiento = models.IntegerField(null=True, blank=True) 
 
     class Meta:
         db_table = 'Rendimiento_Producto'
